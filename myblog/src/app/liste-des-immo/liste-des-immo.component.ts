df15dbaf25d865d6292e271c94513f49fa2ee65a
SibaMarcel <ssibamarcel27@gmail.com>
2022-03-28 17:53:37 +0200

    version 1

---
+++ myblog/src/app/liste-des-immo/liste-des-immo.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'app-liste-des-immo',
+    templateUrl: './liste-des-immo.component.html',
+    styleUrls: []
+})
+
+export class ListeDesImmoComponent {
+    public title = 'Blog des Immobiliers';
+
+    public immobilliers: any[] = [
+        {
+            immoId: 1,
+            immoName: 'Immeuble Kassa',
+            decription: 'Vue sur ambassade des USA',
+            lieu: 'Kobaya',
+            price: 265000,
+            imageUrl: 'assets/img/1.jpg'
+        }, {
+            immoId: 2,
+            immoName: 'Immeuble Kelitigui',
+            decription: 'Vue sur le stade de Nongö',
+            lieu: 'Simbaya',
+            price: 215000,
+            imageUrl: 'assets/img/2.jpg'
+        }, {
+            immoId: 3,
+            immoName: 'Immeuble Magassouba',
+            decription: 'sis au quartier Damond Plaza',
+            lieu: 'Matam',
+            price: 165000,
+            imageUrl: 'assets/img/3.jpg'
+        }, {
+            immoId: 4,
+            immoName: 'Immeuble Tchenguel',
+            decription: 'Vue sur la lagune',
+            lieu: 'Matam',
+            price: 245000,
+            imageUrl: 'assets/img/4.jpg'
+        }, {
+            immoId: 5,
+            immoName: 'Immeuble Ernesto',
+            decription: 'Vue sur la mer',
+            lieu: 'Matam',
+            price: 261000,
+            imageUrl: 'assets/img/5.jpg'
+        }
+    ];
+   
+}
